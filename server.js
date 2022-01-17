df447c93fbd4245dce6660e6e7107b87a5395036
rpatida <raj.patidar02@gmail.com>
2022-01-18 00:13:22 +0530

    initial

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
+const PORT = process.env.PORT || 3300;
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+// set template engine
+console.log(path.join(__dirname, "/resources/views"));
+app.use(expressLayouts);
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
+
+app.listen(PORT, () => {
+  console.log(`Listing on port ${PORT}`);
+});
