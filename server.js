e4c4604a226071c985579505b318c144ac48ff37
rpatida <raj.patidar02@gmail.com>
2022-01-18 12:16:11 +0530

    make home page

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const expressLayouts = require("express-ejs-layouts");
 const path = require("path");
 const PORT = process.env.PORT || 3300;
 
+//Assets
+app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.render("home");
 });
